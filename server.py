6f5c9fb261874f77d0a6f709bcbff6934883e87c
hugop <hugopereirafranco47@gmail.com>
2026-03-20 10:25:37 -0300

    dentro de la funcion handle_client, recibimos el primer mensaje "HEADER" que tiene una longitud predeterminada de 64bytes, esa informacion de bytes la pasamos a formato UTF-8, y luego se imprime el mensaje con el addr del client

--- server.py
+++ server.py
@@ -2,16 +2,26 @@ import socket
 import threading # trabaja con un hilo por cada uno de los clientes
 
 # lo primero que hay que hacer es definir un puerto y la ip del servidor
+HEADER = 64 # el primer mensaje que se reciba del cliente al servidor va a ser de 64 bytes "hello                    " relleno hasta 64 bytes
 PORT = 5050 # es un puerto que se asocia comunmente a aplicaciones de comunicacion 
 SERVER = socket.gethostbyname(socket.gethostname()) # obtiene la direccion de ip local de mi servidor automaticamente (obtener la direccion IPV4 apartir del hotname de la pc)
 ADDR = (SERVER,PORT) #un tupla donde primero tenemos la direccion IP del servidor y el puerto del servidor "192.168.0.X:5050"
+FORMAT = 'uft-8'
 print(SERVER)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # definimos con que tipo de direccion IP vamos a contactar, y como se va a transmitir esos datos
 server.bind(ADDR) # enlazamos el socket a la direccion IP y el PORT del servidor
 
 def handle_client(conn,addr):
-    pass
+    print(f"[NEW CONNECTION] {addr} connected.") #informacion de quien se conecto al servidor
+
+    connected = True
+
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT) #vamos a recibir un mensaje del cliente, lo recibimos del socket conn (esperamos a que envie un mensaje a traves del socket) formato byte a utf-8
+        msg_length = int(msg_length)
+        msg = conn.recv(msg_length).decode(FORMAT)
+        print(f"[{addr}] {msg}")
 
 def start():
     server.listen() # servidor escuchando peticiones 
